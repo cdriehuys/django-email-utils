0db106d3b3d87d84cea35eda0efec6676478188b
Chathan Driehuys <chathan@driehuys.com>
2019-09-11 20:32:40 -0400

    Fix setup.py requirements.
    The previous change upgrading the supported dependency versions didn't
    update the required Django version in setup.py.

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
 
         # Supported versions of Django
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
 
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -32,12 +32,10 @@ setup(
 
         # Supported versions of Python
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # Include the actual source code
@@ -46,6 +44,6 @@ setup(
 
     # Dependencies
     install_requires=[
-        'Django >= 1.10',
+        'Django >= 2.1',
     ],
 )
